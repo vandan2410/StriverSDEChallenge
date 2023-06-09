cc77c43a95bc702e5a41cb6b644271ef5d241d62
vandan2410 <89760982+vandan2410@users.noreply.github.com>
2023-06-09 17:22:46 +0530

    6/9/2023

---
+++ CycleDetectionInLinkedList.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+
+
+bool detectCycle(Node *head)
+{
+	Node* slow=head,*fast=head;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
